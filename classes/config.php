b3dd2f423e5d52d889273dde08f61940f092c474
Mitchell M <mitchell.murphy96@gmail.com>
2018-03-01 15:18:56 -0500

    Structure for backend + some classes

---
+++ classes/config.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @author Mitchell M. 
+ * @version 1.0.0
+ */
+session_start();
+define('DB_HOST', '');
+define('DB_USER', '');
+define('DB_PASS', '');
+define('DB_NAME', '');
+define('SESSION_LENGTH', 60);
+error_reporting(E_ALL & ~E_NOTICE);
+?>
