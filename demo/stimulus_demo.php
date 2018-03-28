c81fcc1dc7ad75b5c2f39c7bddddd5e123735b2c
Mitchell M <mitchell.murphy96@gmail.com>
2018-03-28 10:42:11 -0400

    Deleted old model

--- demo/stimulus_demo.php
+++ demo/stimulus_demo.php
@@ -16,13 +16,12 @@ $session = new Session($dbc);
 ?>
 <html>
 <head>
-<script
-	src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-	<script type="text/javascript">
+<script	src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script type="text/javascript">
 	$(function() {
 	    alert( "ready!" );
 	});
-	</script>
+</script>
 </head>
 <!-- creating stimulus set -->
 <table>
