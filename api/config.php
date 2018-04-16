f2f07638315b7256a72f70c79b29c2c0b61c6730
Dan Blocker <db04839@georgiasouthern.edu>
2018-04-16 04:07:00 -0400

    Update config.php
    kinda had my password on there

--- api/config.php
+++ api/config.php
@@ -6,7 +6,7 @@
 session_start();
 define('DB_HOST', '127.0.0.1');
 define('DB_USER', 'root');
-define('DB_PASS', 'R3dn3ck0065!');
+define('DB_PASS', '');
 define('DB_NAME', 'sosa');
 define('SESSION_LENGTH', 60);
 error_reporting(E_ALL & ~E_NOTICE);
