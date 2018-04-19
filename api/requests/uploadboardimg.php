8864b3a5271c9dd2a5c8bd61398bd6fc873c157e
Mitchell M <mitchell.murphy96@gmail.com>
2018-04-19 01:22:31 -0400

    Extension fix
    Wasn't saving the file extension

--- api/requests/uploadboardimg.php
+++ api/requests/uploadboardimg.php
@@ -8,7 +8,8 @@ if ( 0 < $_FILES['file']['error'] ) {
 	echo 'Error: ' . $_FILES['file']['error'] . '<br>';
 }
 else {
-	$path = dirname(__FILE__) . "/../../board_images/" .$session->generateRandID(8);
+	$parts = explode(".", $_FILES['file']['name']);
+	$path = dirname(__FILE__) . "/../../board_images/". $session->generateRandID(8) + $parts[1];
 	move_uploaded_file($_FILES['file']['tmp_name'], $path);
 	$session->saveBoardImage($board_name,$path);
 	echo "Board image uploaded successfully!";
