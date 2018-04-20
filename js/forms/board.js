6b8b837eacff4dc3bb1369e4bef265ac5134b438
Mitchell M <mitchell.murphy96@gmail.com>
2018-04-20 18:47:46 -0400

    Loading color hex fix

--- js/forms/board.js
+++ js/forms/board.js
@@ -10,12 +10,34 @@ var backgroundColor = " ";
 var cover_Color = " ";
 var image = null;
 
-var fullColorHex = function(r,g,b) {   
-	var red = rgbToHex(r);
-	var green = rgbToHex(g);
-	var blue = rgbToHex(b);
-	return red+green+blue;
-};
+function getBoardHex(){
+
+	var redHex = rgbToHex(document.getElementById("BoardRvalue").value);
+	var greenHex = rgbToHex(document.getElementById("BoardGvalue").value);
+	var blueHex = rgbToHex(document.getElementById("BoardBvalue").value);
+	BoardColor = "0x" + redHex + greenHex + blueHex;
+
+	return BoardColor;
+}
+
+function getBackgroundHex() {
+
+	var redHex = rgbToHex(document.getElementById("BackgroundRvalue").value);
+	var greenHex = rgbToHex(document.getElementById("BackgroundGvalue").value);
+	var blueHex = rgbToHex(document.getElementById("BackgroundBvalue").value);
+	var stringHex = "0x" + redHex + greenHex + blueHex;
+	return stringHex;
+}
+
+function getBoardCoverHex(){
+	var redHex = rgbToHex(document.getElementById("CoverRvalue").value);
+	var greenHex = rgbToHex(document.getElementById("CoverGvalue").value);
+	var blueHex = rgbToHex(document.getElementById("CoverBvalue").value);
+	BoardCoverColor = "0x" + redHex + greenHex + blueHex;
+
+	return BoardCoverColor;
+}
+
 $("button#saveCreatedBoard").on('click', function(){
 	boardName = $("input#board_name").val();
 	lockTilt = $("input#tiltSet").is(":checked");
@@ -23,58 +45,60 @@ $("button#saveCreatedBoard").on('click', function(){
 	lock_zoom = $("input#zoomSet").is(":checked");
 	cover_board = $("input#coverSet").is(":checked");
 
-    var camerax = BoardCamera.position.x;
-    var cameray = BoardCamera.position.y;
-    var cameraz = BoardCamera.position.z;
-	
+	var camerax = BoardCamera.position.x;
+	var cameray = BoardCamera.position.y;
+	var cameraz = BoardCamera.position.z;
+
 	board_color = [$("#BoardRvalue").val(), $("#BoardGvalue").val() , $("#BoardBvalue").val()];
 	backgroundColor = [$("#BackgroundRvalue").val(), $("#BackgroundGvalue").val() , $("#BackgroundBvalue").val()];
 	cover_Color = [$("#CoverRvalue").val(), $("#CoverGvalue").val(), $("#CoverBvalue").val()];
-	
-	var hexBoard = fullColorHex(board_color[0],board_color[1],board_color[2]);
-	var hexBackground = fullColorHex(backgroundColor[0],backgroundColor[1], backgroundColor[2]);
-	var hexCover = fullColorHex(cover_Color[0], cover_Color[1], cover_Color[2]);
+
+
+
+	var hexBoard = getBoardHex();
+	var hexBackground = getBackgroundHex();
+	var hexCover = getBoardCoverHex();
 
 	image = null;
 	var json = {"boardName": boardName, "lock_tilt": lockTilt, "lock_rotate": lockRotate, "lock_zoom": lock_zoom, "board_color": hexBoard,
 			"background_color": hexBackground, "cover_color": hexCover, "image": image, "cover_board": cover_board,"camerax" : camerax, "cameray" : cameray, "cameraz" : cameraz};
 	var packet = JSON.stringify(json);
-	
-		$.ajax({
-			type: 'POST',
-			data: 'request=saveboard&board=' + packet,
-			url: 'api/index.php',
-			async : true,
-			dataType: 'json',
-			success: function (json) {
-				if($('#boardImg').val()) {
-					var file_data = $('#boardImg').prop('files')[0];   
-					var form_data = new FormData();                  
-					form_data.append('file', file_data);
-					form_data.append('request','uploadboardimg');
-					form_data.append('board_name',boardName);
-					alert(form_data);    
-					$.ajax({
-						url: 'api/index.php',
-						dataType: 'text',
-						cache: false,
-						contentType: false,
-						processData: false,
-						data: form_data,                         
-						type: 'post',
-						success: function(php_script_response){
 
-						loadBoardModal();
-						}
-					});
-				} else {
+	$.ajax({
+		type: 'POST',
+		data: 'request=saveboard&board=' + packet,
+		url: 'api/index.php',
+		async : true,
+		dataType: 'json',
+		success: function (json) {
+		if($('#boardImg').val()) {
+			var file_data = $('#boardImg').prop('files')[0];   
+			var form_data = new FormData();                  
+			form_data.append('file', file_data);
+			form_data.append('request','uploadboardimg');
+			form_data.append('board_name',boardName);
+			alert(form_data);    
+			$.ajax({
+				url: 'api/index.php',
+				dataType: 'text',
+				cache: false,
+				contentType: false,
+				processData: false,
+				data: form_data,                         
+				type: 'post',
+				success: function(php_script_response){
 
-					loadBoardModal();
-				}
-			},
-			error: function() {
-				alert("Failed!");
+				loadBoardModal();
 			}
+			});
+		} else {
+
+			loadBoardModal();
+		}
+	},
+	error: function() {
+		alert("Failed!");
+	}
 	});
 });
 
@@ -87,45 +111,47 @@ $("button#loadCreatedBoard").on('click', function(){
 		async : true,
 		dataType: 'json',
 		success: function (json) {
-			var len = objLength(json);
-			
-			var background_hex = json[0].background_color;
-			var board_hex = json[0].board_color;
-			var board_name  = json[0].board_name;
-			var cover_hex = json[0].cover_color;
-			var lock_tilt =  json[0].lock_tilt;
-			var lock_rotate =  json[0].lock_rotate;
-			var lock_zoom =  json[0].board_color;
-			var cover_board = json[0].cover_board;
-			var camerax =  json[0].camerax;
-			var cameray =  json[0].cameray;
-			var cameraz =  json[0].cameraz;
-			
-			$("input#tiltSet").prop('checked', Boolean(lock_tilt));
-			$("input#rotateSet").prop('checked', Boolean(lock_rotate));
-			$("input#zoomSet").prop('checked', Boolean(lock_zoom));
+		var len = objLength(json);
 
-			if(Boolean(lock_tilt))
-				setTiltLock();
-			if(Boolean(lock_rotate))
-				setRotLock();
-			if(Boolean(lock_zoom))
-				setZoomLock();
-			
-			BoardCamera.position.set(camerax,cameray,cameraz);
-			boardName = $("input#board_name").val(board_name);
-			Boardmaterial.color.setHex(background_hex);
-			skyBoxMaterial.color.setHex(background_hex);
-			if(cover_board == 1) {
-				Boardmaterial.color.setHex(cover_hex);
-			}
-			
-			/*for (var i = 0; i < lend; i++) {
+		var background_hex = json[0].background_color;
+		var board_hex = json[0].board_color;
+		var cover_hex = json[0].cover_color;
+
+		var board_name  = json[0].board_name;
+		var lock_tilt =  json[0].lock_tilt;
+		var lock_rotate =  json[0].lock_rotate;
+		var lock_zoom =  json[0].lock_zoom;
+		var cover_board = json[0].cover_board;
+		var camerax =  json[0].camerax;
+		var cameray =  json[0].cameray;
+		var cameraz =  json[0].cameraz;
+
+		$("input#tiltSet").prop('checked', Boolean(lock_tilt));
+		$("input#rotateSet").prop('checked', Boolean(lock_rotate));
+		$("input#zoomSet").prop('checked', Boolean(lock_zoom));
+
+		if(Boolean(lock_tilt))
+			setTiltLock();
+		if(Boolean(lock_rotate))
+			setRotLock();
+		if(Boolean(lock_zoom))
+			setZoomLock();
+
+		BoardCamera.position.set(camerax,cameray,cameraz);
+		boardName = $("input#board_name").val(board_name);
+		Boardmaterial.color.setHex(board_hex);
+		skyBoxMaterial.color.setHex(background_hex);
+		if(cover_board == 1) {
+			Boardmaterial.color.setHex(cover_hex);
+		}
+
+		console.log(board_color);
+		/*for (var i = 0; i < lend; i++) {
 				appendLabel = appendLabel + "<option>"+json[i].title+"</option>";
 			}*/
-		},
-		error: function() {
-		}
+	},
+	error: function() {
+	}
 	});
 });
 
