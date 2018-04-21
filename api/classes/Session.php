7756024b9be688d7b9254c4bd5288cd53b01dc1f
Mitchell M <mitchell.murphy96@gmail.com>
2018-04-21 17:11:43 -0400

    Versioning

--- api/classes/Session.php
+++ api/classes/Session.php
@@ -5,7 +5,7 @@
  * @category   Class
  * @package    com.SOSA-Web.classes.Session
  * @author     Mitchell M. <mm11096@georgiasouthern.edu>
- * @version    Release: 1.5.0
+ * @version    Release: 2.0.0
  * @since      Class available since Release 1.0.0
  */
 require_once __DIR__ . '/../config.php';
