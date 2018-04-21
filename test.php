1b25d4584af9cb40acd275b109f22b4da2cc60c9
jsli4003 <jsli4003@gmail.com>
2018-04-21 16:32:33 -0400

    corrected typo

--- test.php
+++ test.php
@@ -1,2 +1,15 @@
 <?php
-echo phpinfo();
+require_once(__DIR__ . '/config.php');
+
+function __autoload($class_name) {
+    require_once(__DIR__ . '/classes/' . $class_name . '.php');
+}
+
+$db = Database::getConnection();
+$session = Session::getInstance($db);
+echo $session->createExperiment(43,26,"exper1",1,1,"null");
+
+echo "<hr/>";
+echo"<pre>";
+echo var_dump($session->loadExperiment("Y711bHeDKWWW7xQ"));
+echo"</pre>";
