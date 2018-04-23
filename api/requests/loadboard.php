a6d9b82ed4abfbd85b424beba3c4a5c670b160ed
Mitchell M <mitchell.murphy96@gmail.com>
2018-04-23 18:09:34 -0400

    Board Prog

--- api/requests/loadboard.php
+++ api/requests/loadboard.php
@@ -3,5 +3,7 @@ require_once("base.php");
 //all post variables are saved as $key = $val;
 //IE $_POST['username'] accessible here as $username
 //echo $label . " , " . $peg_r . " , " . $peg_g . " , " . $peg_b . " , " . $label_r . " , " . $label_g . " , " . $label_b . " , " . $set_title;
+if(!isset($board))
+	$board = $idboard;
 $array = $session->loadBoard($board);
 echo json_encode($array, JSON_FORCE_OBJECT);
